c78aee81720c6cf9995819e6be9c299de92a9559
Kliukhin Konstantin <kliukhinkonstantin@gmail.com>
2024-05-09 22:50:35 +0300

    feat(middleware): refactored middleware to be more clear, convenient and maintainable

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,11 @@
-import { NextResponse } from "next/server";
 import { withAuth } from "next-auth/middleware";
 
-import { appRoutes } from "@/shared/config/app-routes";
+import { routesRulesConfig, runRoutesMiddleware } from "@/app/rules";
 import { envServer } from "@/shared/config/env-server";
-
-const authRoutes = [appRoutes.signUp, appRoutes.signIn];
 
 export default withAuth(
   async function middleware(req) {
-    const nextUrl = req.nextUrl.pathname;
-    const sessionUser = req.nextauth.token?.user;
-    const isAuthorized = Boolean(sessionUser);
-    const isAuthRoute = authRoutes.includes(nextUrl);
-
-    if (isAuthorized && isAuthRoute) {
-      return NextResponse.redirect(new URL(appRoutes.dashboard, req.url));
-    }
-
-    if (!isAuthorized && !isAuthRoute) {
-      return NextResponse.redirect(new URL(appRoutes.signIn, req.url));
-    }
+    return runRoutesMiddleware(req, routesRulesConfig);
   },
   {
     secret: envServer.NEXTAUTH_SECRET,
