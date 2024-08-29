cdac4abb35c455d0bfe156fe61acf645f79bbfbf
Kostiantyn Kliukhin <kostiantyn.kliukhin@codeandcakes.com>
2024-08-30 00:36:10 +0300

    feat(env): removed deprecated env variables

--- src/shared/lib/add-cookie.ts
+++
@@ -1,7 +0,0 @@
-import { getIsClient } from "./get-is-client";
-
-export function addCookie(name: string, value: string, expires: string) {
-  if (!getIsClient()) return;
-
-  document.cookie = name + "=" + value + "; expires=" + expires + "; path=/;secure=true";
-}
